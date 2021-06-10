da3cfe2aaef6b440b9cb0d2e0fe9e5ba63a05aef
maksud9998 <bmaksud41@gmail.com>
2021-06-11 02:51:14 +0530

    All Designing Done Except Download Fragment. No Idea About It

--- app/src/main/java/com/example/saveittest/MainActivity.java
+++ app/src/main/java/com/example/saveittest/MainActivity.java
@@ -10,35 +10,35 @@ import android.view.MenuItem;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-
-
     BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getSupportFragmentManager().beginTransaction().replace(R.id.frameContainer,new homeFragment()).commit();
+        getSupportFragmentManager().beginTransaction().replace(R.id.frameContainer,new youtubeFragment()).commit();
         bottomNavigationView= findViewById(R.id.bottom_navigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                Fragment temp = null;
+                Fragment selectedFragment = null;
 
                 switch (item.getItemId()){
-                    case R.id.menu_home : temp = new homeFragment();
+                    case R.id.menu_youtube:
+                        selectedFragment = new youtubeFragment();
                     break;
-                    case R.id.menu_youtube: temp = new youtubeFragment();
+                    case  R.id.menu_facebook:
+                        selectedFragment = new facebookFragment();
                     break;
-                    case  R.id.menu_facebook: temp = new facebookFragment();
+                    case R.id.menu_instagram:
+                        selectedFragment = new  instagramFragment();
                     break;
-                    case R.id.menu_instagram: temp = new  instagramFragment();
-                    break;
-                    case  R.id.menu_download: temp = new downloadFragment();
+                    case  R.id.menu_download:
+                        selectedFragment = new downloadFragment();
                     break;
                 }
-                getSupportFragmentManager().beginTransaction().replace(R.id.frameContainer,temp).commit();
-                return false;
+                getSupportFragmentManager().beginTransaction().replace(R.id.frameContainer,selectedFragment).commit();
+                return true;
             }
         });
     }
